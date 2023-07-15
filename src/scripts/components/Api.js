885a739fe5dce26e2a15d883c274ec033975922c
FOXHound <4216623@gmail.com>
2023-07-15 23:08:57 +0300

    upgrade async

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -12,80 +12,79 @@ export class Api {
 
 	}
 
-	getUserInfo() {
-		return fetch(`${this._url}/users/me`, {
+	async getUserInfo() {
+		const res = await fetch(`${this._url}/users/me`, {
 			method: 'GET',
 			headers: this._headers,
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	editUserInfo(data) {
-		return fetch(`${this._url}/users/me`, {
+	async editUserInfo(data) {
+		const res = await fetch(`${this._url}/users/me`, {
 			method: 'PATCH',
 			headers: this._headers,
 			body: JSON.stringify({
 				name: data.name,
 				about: data.job,
 			})
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	getInitialCards() {
-		return fetch(`${this._url}/cards`, {
+	async getInitialCards() {
+		const res = await fetch(`${this._url}/cards`, {
 			method: 'GET',
 			headers: this._headers,
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	addCard(item) {
-		return fetch(`${this._url}/cards`, {
+	async addCard(item) {
+		const res = await fetch(`${this._url}/cards`, {
 			method: 'POST',
 			headers: this._headers,
 			body: JSON.stringify({
 				name: item.name,
 				link: item.link
 			})
-		})
-			.then(res => this._getResponse(res)
-			)
+		});
+		return this._getResponse(res);
 	}
 
-	changeAvatar(data) {
-		return fetch(`${this._url}/users/me/avatar`, {
+	async changeAvatar(data) {
+		const res = await fetch(`${this._url}/users/me/avatar`, {
 			method: 'PATCH',
 			headers: this._headers,
 			body: JSON.stringify({
 				avatar: data.avatar
 			})
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	deleteCard(cardId) {
-		return fetch(`${this._url}/cards/${cardId}`, {
+	async deleteCard(cardId) {
+		const res = await fetch(`${this._url}/cards/${cardId}`, {
 			method: 'DELETE',
 			headers: this._headers,
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	addLike(cardId) {
-		return fetch(`${this._url}/cards/${cardId}/likes`, {
+	async addLike(cardId) {
+		const res = await fetch(`${this._url}/cards/${cardId}/likes`, {
 			method: 'PUT',
 			headers: this._headers,
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
-	deleteLike(cardId) {
-		return fetch(`${this._url}/cards/${cardId}/likes`, {
+	async deleteLike(cardId) {
+		const res = await fetch(`${this._url}/cards/${cardId}/likes`, {
 			method: 'DELETE',
 			headers: this._headers,
-		})
-			.then(res => this._getResponse(res))
+		});
+		return this._getResponse(res);
 	}
 
 
